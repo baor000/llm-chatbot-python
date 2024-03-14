ea40d87e457c1c0e30c1afc0bf49701905ab05a8
phamvanbao <58139951+baor000@users.noreply.github.com>
2024-03-14 10:59:26 +0000

    update format return

--- agent.py
+++ agent.py
@@ -4,27 +4,41 @@ from tools.vector import kg_qa
 from langchain.chains.conversation.memory import ConversationBufferWindowMemory
 from llm import llm
 from langchain.tools import Tool
+from langchain.chains import LLMChain
+from langchain.prompts import PromptTemplate
 
-tools = [
-    Tool.from_function(
-        name="General Chat",
-        description="For general chat not covered by other tools",
-        func=llm.invoke,
-        return_direct=True
-        ),
-    Tool.from_function(
-        name="Vector Search Index",  # (1)
-        description="Provides information about movie plots using Vector Search", # (2)
-        func = kg_qa.invoke, # (3)
-        return_direct=True
-    )
-]
+prompt = PromptTemplate(
+    template="""
+    You are a movie expert. You find movies from a genre or plot.
+
+    Chat History:{chat_history}
+    Question:{input}
+    """,
+    input_variables=["chat_history", "input"],
+)
 
 memory = ConversationBufferWindowMemory(
     memory_key='chat_history',
     k=5,
     return_messages=True,
 )
+
+chat_chain = LLMChain(llm=llm, prompt=prompt, memory=memory)
+
+tools = [
+    Tool.from_function(
+        name="General Chat",
+        description="For general chat not covered by other tools",
+        func=chat_chain.run,
+        return_direct=True
+        ),
+    # Tool.from_function(
+    #     name="Vector Search Index",  # (1)
+    #     description="Provides information about movie plots using Vector Search", # (2)
+    #     func = kg_qa, # (3)
+    #     return_direct=True
+    # )
+]
 
 agent_prompt = hub.pull("hwchase17/react-chat")
 agent = create_react_agent(llm, tools, agent_prompt)
@@ -34,17 +48,15 @@ agent_executor = AgentExecutor(
     memory=memory,
     verbose=True
     )
-
+    
 def generate_response(prompt):
     """
     Create a handler that calls the Conversational agent
     and returns a response to be rendered in the UI
     """
-    prompt = {
-        "input": "What  movie same spot with toy story",
-        # Notice that chat_history is a string, since this prompt is aimed at LLMs, not chat models
-        "chat_history": "Human: Hi! My name is Bob\nAI: Hello Bob! Nice to meet you",
-    }
-    response = agent_executor.invoke(prompt)
-
+    
+    response = agent_executor.invoke({"input": prompt})
+    print(response)
+    if not isinstance(response['output'],str):
+        return response['output'].content
     return response['output']
